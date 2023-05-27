aff247490c8a51d405e1123c5eb6dd00376899f4
Ana Claudia <anacnogueira@gmail.com>
2023-05-27 08:46:06 -0300

    Criando streams de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,22 @@
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+
+class OndeToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+new OndeToHundredStream().pipe(process.stdout);
