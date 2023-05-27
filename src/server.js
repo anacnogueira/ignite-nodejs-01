69c43096127ce964a7208f77cf8437540cdf2861
Ana Claudia <anacnogueira@gmail.com>
2023-05-27 07:39:18 -0300

    Trabalhando com diferentes tipos de método HTTP

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
-  return response.end("Olá JS");
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Criação de usuário");
+  }
+
+  return response.end("Olá mundo");
 });
 
 server.listen(3333);
