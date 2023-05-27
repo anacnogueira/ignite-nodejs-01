56577ad9dc1a00fba8ba636da7aa3106a29158aa
Ana Claudia <anacnogueira@gmail.com>
2023-05-27 07:52:26 -0300

    Setando headers na resposta

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/users") {
-    return response.end("Listagem de usuários");
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "Fulano",
+      email: "fulano@example.com",
+    });
+
     return response.end("Criação de usuário");
   }
 
