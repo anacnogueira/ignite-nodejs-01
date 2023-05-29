e1cfa94185ad754c9c1d17e3ae03b71d33691d5d
Ana Claudia <anacnogueira@gmail.com>
2023-05-29 16:57:47 -0300

    Adicionando rotas

--- src/server.js
+++ src/server.js
@@ -1,38 +1,24 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
+
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
+import { routes } from "./routes.js";
 
-const database = new Database();
-
-const server = http.createServer(async (request, response) => {
-  const { method, url } = request;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
   const buffers = [];
 
-  await json(request, response);
+  await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    return response.end(JSON.stringify(users));
+  if (route) {
+    return route.handler(req, res);
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = request.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return response.writeHead(201).end();
-  }
-
-  return response.writeHead(404).end("Not Found");
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
