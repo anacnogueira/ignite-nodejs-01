141dfb8024f89105797a6e01deea80283d41e168
Ana Claudia <anacnogueira@gmail.com>
2023-05-30 19:04:57 -0300

    Formando a rota de delete com regex

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js";
 import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -29,5 +30,10 @@ export const routes = [
 
       return res.writeHead(201).end();
     },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:uuid"),
+    handler: (req, res) => {},
   },
 ];
