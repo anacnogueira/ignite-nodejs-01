259edb7a89c57692e82f25e82e258aeedf0b47f7
Ana Claudia <anacnogueira@gmail.com>
2023-05-31 11:35:13 -0300

    Filtrando o usuario na lista

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,16 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = database.select("users");
+      const { search } = req.query;
+      const users = database.select(
+        "users",
+        search
+          ? {
+              name: search,
+              email: search,
+            }
+          : null
+      );
 
       return res.end(JSON.stringify(users));
     },
